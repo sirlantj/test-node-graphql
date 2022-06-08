a8ea325c70643f7301e7a439ae6570c8d108a4d5
sirlantj <sirlantj@gmail.com>
2022-06-07 21:16:31 -0300

    Refactored code

--- src/api/graphql/resolvers/cities.ts
+++ src/api/graphql/resolvers/cities.ts
@@ -3,14 +3,15 @@ import { Cities } from '../../../db/models';
 export const citiesResolvers = {
   Query: {
     async getAll() {
-      return Cities.findAll();
+      return await Cities.findAll();
     },
     async getByZipCode(parent: any, args: any, context: any, info: any) {
-      console.log(args);
-      let test = Cities.findOne({ where: { zipcode: args.zipcode } });
-      console.log(test);
-      return test;
-      //return Cities.findOne({ where: { zipcode: zipcode } });
+      let result = await Cities.findOne({ where: { zipcode: args.zipcode } });
+      console.log(result);
+      if (!result) {
+        throw new Error('404');
+      }
+      return result;
     },
   },
 };
